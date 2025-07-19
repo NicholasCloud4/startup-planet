ded492f027cc00008a9f5bed5385dc5d81928d3c
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-07-18 20:55:01 -0400

    Filtering by Query Params

--- server.js
+++ server.js
@@ -5,15 +5,63 @@ const PORT = 8000
 
 const app = express()
 
-/*
-Challenge:
-  1. When the client makes a GET request to ‘/api’, serve all of our data as json.
-
-  hint.md for help!
-*/
 
 app.get('/api', (req, res) => {
-    res.json(startups)
+    let filteredData = startups
+    /*
+    Challenge:
+    1. When a user hits the /api endpoint with query params, filter the data so 
+    we only serve objects that meet their requirements. 
+         
+    The user can filter by the following properties:
+      industry, country, continent, is_seeking_funding, has_mvp
+    
+    Test Cases
+    
+    /api?industry=renewable%20energy&country=germany&has_mvp=true
+      Should get the "GreenGrid Energy" object.
+    
+    /api?industry=renewable energy&country=germany&has_mvp=false
+      Should not get any object
+    
+    /api?continent=asia&is_seeking_funding=true&has_mvp=true
+      should get for objects with IDs 3, 22, 26, 29
+    */
+
+    const { industry, country, continent, is_seeking_funding, has_mvp } = req.query
+
+    if (industry) {
+        filteredData = filteredData.filter(startup =>
+            startup.industry.toLowerCase() === industry.toLowerCase()
+        )
+    }
+
+    if (country) {
+        filteredData = filteredData.filter(startup =>
+            startup.country.toLowerCase() === country.toLowerCase()
+        )
+    }
+
+    if (continent) {
+        filteredData = filteredData.filter(startup =>
+            startup.continent.toLowerCase() === continent.toLowerCase()
+        )
+    }
+
+    if (is_seeking_funding) {
+        filteredData = filteredData.filter(startup =>
+            startup.is_seeking_funding === JSON.parse(is_seeking_funding.toLowerCase())
+        )
+    }
+
+    if (has_mvp) {
+        filteredData = filteredData.filter(startup =>
+            startup.has_mvp === JSON.parse(has_mvp.toLowerCase())
+        )
+    }
+
+
+    res.json(filteredData)
 })
 
 app.listen(PORT, () => console.log(`server connected on port ${PORT}`))
