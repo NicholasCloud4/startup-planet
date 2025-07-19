2f02146d45144e2459661b8d325f2e5e61e52327
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-07-19 19:03:11 -0400

    Add validation for unsupported search fields in dynamic route

--- server.js
+++ server.js
@@ -50,21 +50,33 @@ app.get('/api/:field/:term', (req, res) => {
 
     const { field, term } = req.params
 
-    const filteredData = startups.filter(startup =>
-        startup[field].toLowerCase() === term.toLowerCase()
+    const allowedFields = ['country', 'continent', 'industry']
+
+    if (!allowedFields.includes(field)) {
+        return res.status(400).json({ message: "Search field not allowed. Please use only 'country', 'continent', 'industry'" })
+    }
+
+
+    /*
+    Challenge:
+    1. If the client’s 'field' is not supported, serve this object:
+      {message: "Search field not allowed. Please use only 'country', 'continent', 'industry'" }
+    2. Chain in the .status(<code>) method to set a status code.
+        What status code should you set?
+    3. You might run into an error! Find a solution!
+    
+    hint.md for help!
+    */
+
+    const filteredData = startups.filter(
+        startup => startup[field].toLowerCase() === term.toLowerCase()
     )
 
     res.json(filteredData)
+
 })
 
 /*
-Challenge:
-1. Add a new route which accepts GET requests to /api/<field>/<term>.
-2. Filter the data based on the path params.
-3. Serve the filtered data.
-
-For now, don’t worry that using some fields will trigger an error.
-
 ** The functionality **
 Get all startups in a given country via api/country/<country name>
 Get all startups in a given continent via api/continent/<continent name>
@@ -77,11 +89,9 @@ These should work:
   api/continent/europe
   api/industry/ai
 
-This will throw an error - but that's fine!
+This should return the object given in the challenge above. 
     api/has_mvp/true
 
 */
-
-
 
 app.listen(PORT, () => console.log(`server connected on port ${PORT}`))
